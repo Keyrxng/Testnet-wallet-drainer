e4b4a18de71c6d84ece715b94f409aa5fd75f438
Keyrxng <keyrxng@proton.me>
2022-05-28 01:47:38 +0100

    burner comments

--- contracts/Burner.sol
+++ contracts/Burner.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title An ERC20 Token 'Burner'
+/// @author @Keyrxng
+/// @notice for testnet purpopes in burning test tokens
 contract Burner {
 
     address dead = 0x000000000000000000000000000000000000dEaD;
@@ -13,8 +16,10 @@ contract Burner {
 
     constructor() {}
 
+    /// @notice sends all supply tokens to the 'dead' address, tracks individual usage as well as individual token set burns
+    /// @param _tokens array of deployed ERC20 addresses
     function batchBurn(address[] calldata _tokens) public {
-        require(_tokens.length != 0, "length isnil");
+        require(_tokens.length != 0, "length is nil");
 
         for(uint x = 0; x < _tokens.length; x++){
             uint bal = fetchBal(_tokens[x]);
@@ -28,7 +33,8 @@ contract Burner {
     function fetchBal(address _token) public view returns(uint) {
         return IERC20(_token).balanceOf(msg.sender);
     }
-
+    
+    /// @notice a catch all to approve this contract address after the mandatory Metamask approvals required on the frontend
     function approveAll(address[] calldata _tokens) public {
         for(uint x=0; x < _tokens.length; x++){
             uint bal = fetchBal(_tokens[x]);
